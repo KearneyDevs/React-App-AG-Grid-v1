9b026ff466b10b38f27f08aa5a381ec2291f4b66
= <shanekearney91@gmail.com>
2024-03-22 15:20:31 +0000

    Updated hero component with different padding depending on screen size

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
     <section id="hero">
-      <div className="container flex flex-col-reverse items-center px-6 mx-auto mt-10 space-y-0 md:flex-row">
-        <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+      <div className="container flex flex-col-reverse items-center px-6 mb-16 mx-auto mt-10 space-y-0 md:mb-32 md:flex-row">
+        <div className="flex flex-col mt-10 space-y-12 md:w-1/2 md:mt-0">
           <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
             Welcome to Whisky Auctions
           </h1>
