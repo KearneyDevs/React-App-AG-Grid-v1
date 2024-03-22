e8db2052f548efaff00181861c5bd7237f233726
= <shanekearney91@gmail.com>
2024-03-22 16:15:53 +0000

    populated header with newly added routes

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,38 +1,40 @@
 const Header = () => {
   return (
-    <nav className="relative container mx-auto p-6">
-      <div className="flex items-center justify-between">
-        <div>
-          <a href="/" className="font-bold">
-            Whisky Auctions
-          </a>
-        </div>
-        <div className="hidden space-x-6 md:flex">
-          <a href="/" className="hover:text-darkGrayishBlue">
-            Home
-          </a>
-          <a href="/distilleries" className="hover:text-darkGrayishBlue">
-            Distilleries
-          </a>
-          <a href="/" className="hover:text-darkGrayishBlue">
-            Auctions
-          </a>
-          <a href="/" className="hover:text-darkGrayishBlue">
-            About
-          </a>
-          <a href="/" className="hover:text-darkGrayishBlue">
-            Contact
-          </a>
-        </div>
+    <header>
+      <nav className="relative container mx-auto p-6">
+        <div className="flex items-center justify-between">
+          <div>
+            <a href="/" className="font-bold">
+              Whisky Auctions
+            </a>
+          </div>
+          <div className="hidden space-x-6 md:flex">
+            <a href="/" className="hover:text-darkGrayishBlue">
+              Home
+            </a>
+            <a href="/distilleries" className="hover:text-darkGrayishBlue">
+              Distilleries
+            </a>
+            <a href="/auctions" className="hover:text-darkGrayishBlue">
+              Auctions
+            </a>
+            <a href="/about" className="hover:text-darkGrayishBlue">
+              About
+            </a>
+            <a href="/contact" className="hover:text-darkGrayishBlue">
+              Contact
+            </a>
+          </div>
 
-        {/* <a
+          {/* <a
             href="#"
             className="hidden md:block p-3 px-6 text-white bg-brightRed rounded-full align-middle hover:bg-brightRedLight"
           >
             Get Started
           </a> */}
-      </div>
-    </nav>
+        </div>
+      </nav>
+    </header>
   );
 };
 
