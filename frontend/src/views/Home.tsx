3bdedce59679b6ef73e18f915dd255bae6900a68
= <shanekearney91@gmail.com>
2024-03-22 15:49:28 +0000

    Initialised new component call to action and imported it to home component

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero";
 import Stats from "../components/Stats";
 import Process from "../components/Process";
+import CallToAction from "../components/CallToAction";
 const Home = () => {
   return (
     <>
       <Hero />
       <Stats />
       <Process />
+      <CallToAction />
     </>
   );
 };
